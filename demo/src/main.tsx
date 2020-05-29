ffb4c1c535b24c38e242dde241c613985b3e804a
feifan <feifan.gff@alibaba-inc.com>
2020-05-29 21:17:00 +0800

    chore: fix

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { init, createApp } from 'turbox';
+// import { init, createApp } from 'turbox';
 import DemoBox from './component/index';
 
 document.addEventListener("DOMContentLoaded", function (event) {
--- packages/turbox/src/index.ts
+++ packages/turbox/src/index.ts
@@ -1,2 +1,1 @@
 export * from '@turbox3d/reactivity';
-export * from '@turbox3d/atom';
