1f361c31c34d522b620ac6a4f8a3973b4ec4aeca
feifan <feifan.gff@alibaba-inc.com>
2020-07-09 22:19:50 +0800

    chore: add benchmark test code

---
+++ demo/src/benchmark.ts
@@ -0,0 +1,102 @@
+import { reactor, mutation, Domain, reactive, init, config } from 'turbox';
+import { observable, action, autorun } from 'mobx';
+
+class TestTurbox extends Domain {
+  @reactor() a = {
+    a: {
+      a: {
+        a: {
+          a: {
+            a: {
+              a: 0,
+            },
+          },
+        },
+      },
+    },
+  };
+
+  @mutation do() {
+    this.a.a.a.a.a.a.a += 1;
+  }
+
+  @mutation innerDo() {
+    for (let index = 0; index < 1000; index++) {
+      this.a.a.a.a.a.a.a += 1;
+    }
+  }
+}
+
+const td = new TestTurbox();
+
+(() => {
+  /** 关闭时间旅行和 logger，因为 mobx 没有，但是理论上仍然做不到完全公平，这部分是有开销的 */
+  config({
+    timeTravel: {
+      isActive: false,
+    },
+    middleware: {
+      logger: false,
+    }
+  });
+  init();
+  let start, end;
+  reactive(() => {
+    end = performance.now();
+    console.log('turbox:', end - start);
+    console.log('turbox:', td.a.a.a.a.a.a.a);
+  });
+  start = performance.now();
+  /** 1. 多次调用可复用的 mutation */
+  for (let index = 0; index < 1000; index++) {
+    td.do();
+  }
+  /** 2. 循环放在函数里面 */
+  td.innerDo();
+  console.dir(performance.memory);
+})();
+
+class TestMobx {
+  @observable a = {
+    a: {
+      a: {
+        a: {
+          a: {
+            a: {
+              a: 0,
+            },
+          },
+        },
+      },
+    },
+  };
+
+  @action do() {
+    this.a.a.a.a.a.a.a += 1;
+  }
+
+  @action innerDo() {
+    for (let index = 0; index < 1000; index++) {
+      this.a.a.a.a.a.a.a += 1;
+    }
+  }
+}
+
+const tm = new TestMobx();
+
+(() => {
+  let start, end;
+  autorun(() => {
+    end = performance.now();
+    console.log('mobx:', end - start);
+    console.log('mobx:', tm.a.a.a.a.a.a.a);
+  });
+  start = performance.now();
+  /** 1. 多次调用可复用的 action */
+  for (let index = 0; index < 1000; index++) {
+    tm.do();
+  }
+  /** 2. 循环放在函数里面 */
+  tm.innerDo();
+  console.dir(performance.memory);
+})();
--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -1,36 +1,8 @@
-import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-// import { init, createApp } from 'turbox';
+import * as React from 'react';
 import DemoBox from './component/index';
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  // init();
-  // createApp({
-  //   name: 'tpzz',
-  //   meta: {},
-  //   preload: {
-  //     range: 2,
-  //   },
-  //   getConfig: async () => {
-  //     // const configJson = await fetch('osskey');
-  //     // return configJson;
-  //     return {
-  //       'TPZZ_PLUGINA': 'http://localhost:9001/build/bundle.js',
-  //       'TPZZ_PLUGINB': 'http://localhost:9002/build/bundle.js',
-  //     };
-  //   },
-  //   plugins: () => {
-  //     return ['TPZZ_PLUGINA', 'TPZZ_PLUGINB'];
-  //   }
-  // });
-  // createApp({
-  //   name: 'hs',
-  //   meta: {},
-  //   plugins: ['TPZZ_PLUGINA', 'TPZZ_PLUGINB'],
-  // });
-  ReactDOM.render(
-    <DemoBox />,
-    document.getElementById('app')
-  );
-});
-
+ReactDOM.render(
+  <DemoBox />,
+  document.getElementById('app')
+);
