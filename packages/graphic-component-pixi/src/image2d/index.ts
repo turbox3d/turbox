1aa2ed832281016df4c87fb8c542c6de6cb82698
geoffgu <darknight3@vip.qq.com>
2024-04-08 20:49:14 +0800

    fix: fix code style

--- packages/graphic-component-pixi/src/image2d/index.ts
+++ packages/graphic-component-pixi/src/image2d/index.ts
@@ -6,11 +6,11 @@ import { IFitStyle } from '../draw-utils/drawRect';
 import Container2d from '../container2d';
 
 interface IProps {
+  width: number;
+  height: number;
   x?: number;
   y?: number;
   position?: Vec2;
-  width: number;
-  height: number;
   /**
    * 传入的位置坐标是否是矩形中心点
    */
